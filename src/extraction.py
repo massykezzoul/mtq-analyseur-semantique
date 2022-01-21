b8693086becc723c90366ec7284114a1cce6d662
Khaoula-AY <82915030+Khaoula-AY@users.noreply.github.com>
2022-01-21 16:43:00 +0100

    extraction et cache ainsi que la ponctuation

---
+++ src/extraction.py
@@ -0,0 +1,98 @@
+
+  
+import re
+import os
+import pickle
+from unittest import case
+from itertools import groupby
+from operator import itemgetter
+from bs4 import BeautifulSoup
+import requests
+
+# Prend le  mot et retourne grace au  code html correspondant depuis http://www.jeuxdemots.org/rezo-dump et on prendant pour l'instant la relation r_pos telque son poid avec le noeud du mot recercher est maximale.
+
+def extraction_jdm(word: str,rel: str='4'):
+
+    chemin_absolu = os.path.dirname(os.path.abspath(__file__))
+    
+    if ( os.path.isdir(chemin_absolu + '/cache') and os.path.isfile(
+            chemin_absolu + '/cache/' + word + '.pkl')):
+        fichier = open(chemin_absolu + '/cache/' + word + '.pkl', 'rb')
+        categorie = pickle.load(fichier)
+        fichier.close()
+        return categorie;
+    
+    html = requests.get('http://www.jeuxdemots.org/rezo-dump.php?gotermsubmit=Chercher&gotermrel=' + word + '&rel='+rel)
+    encoding = html.encoding if 'charset' in html.headers.get('content-type', '').lower() else None
+    soup = BeautifulSoup(html.content, 'html.parser', from_encoding='iso-8859-1')
+    texte_brut = soup.find_all('code')
+    noeuds = re.findall('[e];[0-9]*;.*', str(texte_brut))
+    relations = re.findall('[r];[0-9]*;.*', str(texte_brut))
+    if ((not noeuds) and (not relations)):
+        print("le mot " + word + " n'existe pas dans jeux de mots")
+        return None
+    
+    tableau_noeuds = []
+    tableau_relations = []
+
+    for noeud in noeuds:
+        noeud = noeud.replace('&lt;', '<')
+        noeud = noeud.replace('&gt;', '>')
+        tableau_noeuds.append(noeud.split(';'))
+    for relation in relations:
+        relation = relation.replace('&lt;', '<')
+        relation = relation.replace('&gt;', '>')
+        tableau_relations.append(relation.split(';'))
+
+    id = {}
+    i = 0
+    while i <= len(tableau_relations) - 1:
+        if (int(tableau_relations[i][5]) >= 0):
+            id[int(tableau_relations[i][3])] = int(tableau_relations[i][5])
+        i += 1
+
+    categorie = []
+    for N in tableau_noeuds:
+        if (int(N[1]) in id):
+            if '>' in N[2] and len(N) >= 6:
+                categorie.append((N[5].replace("'", ''), id[int(N[1])]))
+            else:
+                categorie.append((N[2].replace("'", ''), id[int(N[1])]))
+
+    
+    chemin_absolu = os.path.dirname(os.path.abspath(__file__))
+    if not os.path.isdir(chemin_absolu + '/cache'):
+        try:
+            os.mkdir(chemin_absolu + '/cache')
+        except OSError:
+            print('La création du dossier cache a échoué')
+                
+    fichier_cache = open(chemin_absolu + '/cache/' + word + '.pkl', 'wb')
+    pickle.dump(categorie, fichier_cache)
+    fichier_cache.close()
+        
+    return categorie
+
+def get_pos(categorie):
+    tabPOS = ["Adv", "Adj", "Conj", "Det", "Nom", "Pre", "Pro", "Ver","Punct"]
+
+    categorie_new = [] 
+    for x in categorie:
+        element = x[0].split(':')[0]
+        if element in tabPOS and element not in [y[0] for y in categorie_new]:
+            categorie_new.append((element, x[1]))
+
+    return categorie_new
+
+if __name__ == '__main__':
+    phrase = input("Entrez la phrase : \n")
+    words = phrase.split(" ");
+    for word in words:
+        if (word[len(word) - 1] in ['.', ',', '!', ':', '?', ';']):
+            print(word[:-1] + "  :: ")
+            print(get_pos(extraction_jdm(word[:-1])))
+            print(word[len(word) - 1] + "  :: ")
+            print(get_pos(extraction_jdm(str(word[len(word) - 1]))))
+        else:
+            print(word + "  :: ")
+            print(get_pos(extraction_jdm(word)))
